796fb9c53bab78b64f9a53b30c057278f4b0c616
Lucas Braga <146142175+Lucas-R-Braga@users.noreply.github.com>
2024-03-08 10:51:45 -0300

    Teste do valor de indice
    Primeiro desafio - Teste do valor de indice

---
+++ teste1/TesteIndice.java
@@ -0,0 +1,19 @@
+package teste1;
+
+public class TesteIndice {
+
+    public static void main(String []args){
+        
+        int index = 13;
+        int soma = 0;
+        int k = 0;
+        
+        while (k < index){
+            k = k + 1;
+            soma = soma + k;
+            
+        }
+        System.out.println(soma);
+    }
+    
+}
